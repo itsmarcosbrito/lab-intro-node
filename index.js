7230fa8a9b3868fe2a034f73a03c4de48fdae577
Marcos Brito <marcosbrito@Marcoss-MBP.lan>
2021-02-02 10:02:29 +0000

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
+  constructor() {
+  this.items = [];
+  this.length = this.items.length;
+}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b); //Sort ((a,b) => a-b)
+    this.length ++;  // +1 devido ao add item ao length 
+    return this.items;
+  }
 
   get(pos) {}
 
